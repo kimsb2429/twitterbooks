4715d454d3618533ec3617d91f61331d85e55a15
Jae Kim <jaekim@Jaes-MacBook-Air.local>
2022-01-24 12:52:12 -0600

    First Commit

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
-if __name__ == '__main__':
-    print("hello")
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
