3b60b94524d46dd62c6769d0d0f2b6e2c360872f
Jae Kim <jaekim@Jaes-MacBook-Air.local>
2022-01-24 23:34:47 -0600

    Formatted title and header

--- hello.py
+++ hello.py
@@ -3,13 +3,15 @@ import numpy as np
 import pandas as pd
 import awswrangler as wr
 
+st.title('TWITTERBOOKS')
+st.header('BILLBOARD 100')
+st.header('Week of January 22, 2022')
 df = wr.s3.read_json(path='s3://warcbooks/data/staging/batch/topbooks/topbooks.json', dtype=False)
+st.dataframe(df,800,1200)
+# if st.checkbox('Show dataframe'):
+#    chart_data = df
+#    #  chart_data = pd.DataFrame(
+#    #     np.random.randn(20, 3),
+#    #     columns=['a', 'b', 'c'])
 
-
-if st.checkbox('Show dataframe'):
-   chart_data = df
-   #  chart_data = pd.DataFrame(
-   #     np.random.randn(20, 3),
-   #     columns=['a', 'b', 'c'])
-
-   chart_data
+#    chart_data
