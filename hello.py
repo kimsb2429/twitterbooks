599cf4d77d5494b88658ced0b07306ed2f1d7918
Jae Kim <jaekim@Jaes-MacBook-Air.local>
2022-01-24 23:42:10 -0600

    Formatted header

--- hello.py
+++ hello.py
@@ -4,8 +4,8 @@ import pandas as pd
 import awswrangler as wr
 
 st.title('TWITTERBOOKS')
-st.header('BILLBOARD 100')
 st.header('Week of January 22, 2022')
+# st.header('week of January 22, 2022')
 df = wr.s3.read_json(path='s3://warcbooks/data/staging/batch/topbooks/topbooks.json', dtype=False)
 st.dataframe(df,800,1200)
 # if st.checkbox('Show dataframe'):
