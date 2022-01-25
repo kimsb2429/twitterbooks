03fde2765c97f50cf22fb6695f71558afe970c60
Jae Kim <jaekim@Jaes-MacBook-Air.local>
2022-01-25 16:38:09 -0600

    Added charts

--- hello.py
+++ hello.py
@@ -1,19 +1,70 @@
-import streamlit as st
 import numpy as np
 import pandas as pd
 import awswrangler as wr
-from PIL import Image
+import streamlit as st
+import plotly
+import plotly.figure_factory as ff
+import datetime
 
+# wide mode
+st.set_page_config(layout="wide")
 st.title('TWITTERBOOKS')
 st.header('Week of January 22, 2022')
-# st.header('week of January 22, 2022')
-df = wr.s3.read_json(path='s3://warcbooks/data/staging/batch/topbooks/topbooks.json', dtype=False)
-df = df.to_html(escape=False, index=False)
-st.dataframe(df,800,1200)
-# if st.checkbox('Show dataframe'):
-#    chart_data = df
-#    #  chart_data = pd.DataFrame(
-#    #     np.random.randn(20, 3),
-#    #     columns=['a', 'b', 'c'])
 
-#    chart_data
+# two-column layout
+col1, col1x, col2, = st.columns([1.5,0.05, 1])
+df = wr.s3.read_json(path='s3://warcbooks/data/staging/batch/topbooks/topbooks.json', dtype=False)
+years = df['year'].astype(int).tolist()
+min_year = min(years)
+max_year = datetime.date.today().year
+np_years = np.array(years)
+min_default = int(np.quantile(np_years,0.25))
+max_default = int(np.quantile(np_years,0.75))
+
+# slider to select year range
+values = col2.slider(
+     'Filter by years',
+     min_year, max_year, (min_default,max_default))
+col2.text("")
+
+
+# the main data table
+col1.dataframe(df,height=750)
+
+# group mention counts by year and by the slider selection
+yeardf = df[['year','mentions']].groupby(['year']).sum()
+yeardf = yeardf.reset_index()
+yeardf['year'] = yeardf['year'].astype(int)
+group = []
+for row in yeardf['year']:
+   if row<values[0]:
+      group.append('pre')
+   elif (row>=values[0]) & (row<=values[1]):
+      group.append('mid')
+   else:
+      group.append('post')
+yeardf['group']=group
+
+# barchart 1
+mid = yeardf[yeardf['group']=='mid']
+mid_indexed = mid.groupby(['year']).sum()
+col2.bar_chart(mid_indexed)
+
+
+# barchart 2
+pmp = yeardf.groupby('group').mentions.transform(np.sum).drop_duplicates().tolist()
+if len(pmp)==1:
+   year_range = [f'{min_year} - {max_year}']
+elif len(pmp)==2:
+   if min_year == values[0]:
+      year_range = [f'{min_year} - {str(values[1])}', f'{str(values[1]+1)} - {max_year}']   
+   else:
+      year_range = [f'{str(values[0])} - {str(values[1]-1)}', f'{str(values[1])} - {max_year}']   
+else:
+   year_range = [f'{min_year} - {str(values[0]-1)}', f'{str(values[0])} - {str(values[1])}', f'{str(values[1]+1)} - {max_year}']
+pre_mid_post = pd.DataFrame()
+pre_mid_post['year_range'] = year_range
+pre_mid_post['mentions'] = [x/sum(pmp) for x in pmp]
+pre_mid_post = pre_mid_post.groupby(['year_range']).sum()
+col2.bar_chart(pre_mid_post)
+
