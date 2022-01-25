3114c5e5344694e8b908d6222759470f9d91fe72
Jae Kim <jaekim@Jaes-MacBook-Air.local>
2022-01-24 19:16:01 -0600

    Testing AWS connection

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import awswrangler as wr
+
+df = wr.s3.read_json(path='s3://warcbooks/data/staging/batch/topbooks/topbooks.json', dtype=False)
+
 
 if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
+   chart_data = df
+   #  chart_data = pd.DataFrame(
+   #     np.random.randn(20, 3),
+   #     columns=['a', 'b', 'c'])
 
-    chart_data
+   chart_data
